67f55a44c81fb76e246d4e120267335b7423782d
fejq <przecinek45@o2.pl>
2018-06-27 15:09:22 +0200

    added passing form data through service

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,16 +1,26 @@
-import { Component, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs'
 import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent {
-  // posts = [
-  //   {title: 'First post', content: 'This is the first post content'},
-  //   {title: 'Second post', content: 'This is the second post content'},
-  //   {title: 'Third post', content: 'This is the third post content'},
-  // ];
-  @Input() posts: Post[] = [];
+export class PostListComponent implements OnInit, OnDestroy {
+  posts: Post[] = [];
+  private postsSub: Subscription;
+
+  constructor(public postService: PostsService) {}
+
+  ngOnInit() {
+    this.postsSub = this.postService.getPostUpdateListener().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+  }
 }
