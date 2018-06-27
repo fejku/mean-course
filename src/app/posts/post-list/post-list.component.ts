0c75fd58e43af6291f11e5f80345b6a6fce7b55c
fejq <przecinek45@o2.pl>
2018-06-27 14:38:09 +0200

    working passing form info from one component to the other before service

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { Post } from '../post.model';
+
+@Component({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  // posts = [
+  //   {title: 'First post', content: 'This is the first post content'},
+  //   {title: 'Second post', content: 'This is the second post content'},
+  //   {title: 'Third post', content: 'This is the third post content'},
+  // ];
+  @Input() posts: Post[] = [];
+}
